c17562b7bcd57f63d3e24005e7464cc5e11d05e0
ntc-ai <mikkel+nanosapiangithub@255bits.com>
2023-05-04 00:24:14 -0600

    extract

--- docker/extract_lora.sh
+++ docker/extract_lora.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+cd /sd-scripts
+source lora/bin/activate
 # Set the directory path
-dir="../../stable-diffusion-webui2/models/Stable-diffusion/0new/"
-mv -v ../models/*/*.ckpt ../../stable-diffusion-webui2/models/Stable-diffusion/0new
+dir="/stable-diffusion-webui/models/Stable-diffusion/conceptmod"
+mkdir "$dir"
+mv -v models/*/*.ckpt "$dir"
 
 # Loop through all files in the directory
 for file in "${dir}"*; do
@@ -13,7 +16,7 @@ for file in "${dir}"*; do
     clean_filename=$(echo "${filename}" | tr -d '|~#:')
 
     # Set the target file path
-    target="../../stable-diffusion-webui2/models/Lora/0new/${clean_filename}.safetensors"
+    target="$dir/${clean_filename}.safetensors"
 
     # Check if the target file exists; if it does, skip the command
     if [ ! -f "${target}" ]; then
@@ -21,9 +24,48 @@ for file in "${dir}"*; do
         python3 networks/extract_lora_from_models.py \
             --save_precision fp16 \
             --save_to "${target}" \
-            --model_org ../../stable-diffusion-webui2/models/Stable-diffusion/colorful_v23.safetensors \
+            --model_org $model \
             --model_tuned "${file}"
     else
         echo "Skipping ${filename}, target file already exists."
     fi
 done
+
+
+#!/bin/bash
+
+# Set the default model path
+default_model_path="/workspace/stable-diffusion-webui/models/Stable-diffusion/SDv1-5.ckpt"
+
+# Define $model as the first argument passed in or use the default model path
+model="${1:-$default_model_path}"
+
+# Set the directory path
+dir="/stable-diffusion-webui/models/Stable-diffusion/conceptmod"
+mkdir "$dir"
+mv -v models/*/*.ckpt "$dir"
+
+# Loop through all files in the directory
+for file in "${dir}/"*; do
+    # Extract the filename without extension
+    filename=$(basename -- "${file%.*}")
+
+    # Remove special characters from the filename
+    clean_filename=$(echo "${filename}" | tr -d '|~#:')
+
+    # Set the target file path
+    target="$dir/${clean_filename}.safetensors"
+
+    # Check if the target file exists; if it does, skip the command
+    if [ ! -f "${target}" ]; then
+        # Run the command with the required arguments
+        python3 networks/extract_lora_from_models.py \
+            --save_precision fp16 \
+            --save_to "${target}" \
+            --model_org "${model}" \
+            --model_tuned "${file}"
+    else
+        echo "Skipping ${filename}, target file already exists."
+    fi
+done
+
