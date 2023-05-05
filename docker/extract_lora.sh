ed1f1af92c27bc92d2db839b44de2fd43698450f
ntc-ai <mikkel+nanosapiangithub@255bits.com>
2023-05-04 22:32:29 -0600

    dockerfile fixes

--- docker/extract_lora.sh
+++ docker/extract_lora.sh
@@ -25,9 +25,7 @@ for file in "${dir}/"*; do
     # Remove special characters from the filename
     clean_filename=$(echo "${filename}" | tr -d '|~#:')
 
-    dir_replaced=$(echo "$dir" | sed 's/\/models\/Stable-diffusion\/conceptmod\//\/Lora\//')
-    mkdir -p "$dir_replaced"
-    target="$dir_replaced/${clean_filename}.safetensors"
+    target="/workspace/stable-diffusion-webui/Lora/${clean_filename}.safetensors"
 
     # Check if the target file exists; if it does, skip the command
     if [ ! -f "${target}" ]; then
